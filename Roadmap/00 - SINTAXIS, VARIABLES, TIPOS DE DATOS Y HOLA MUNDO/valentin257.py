e257338b950bdab73ff472e9e6a51f2655df9967
valentin257 <valentin.bertonati@outlook.com>
2024-10-08 18:44:02 -0300

    Create valentin257.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/valentin257.py
@@ -0,0 +1,75 @@
+# https://python.org/ 
+
+# Esto es un comentario de una sola línea
+# UwU
+"""
+Esto tambien es un comentario
+pero
+En varias líneas
+Fua que increíble
+"""
+
+'''
+Así tambien son los comentarios
+en varias lineas
+pero en vez de comillas dobles
+usé comillas simples
+:o
+'''
+#Vamos a crear una variable
+
+la_variable = "Variable"  
+
+#Constante, no se puede crear en python
+
+CONSTANTE = "Constante" #las mayusculas indican una constante aunque no sea propiamente una y pueda variar.
+
+la_variable = "Varió"
+
+#Tipos de datos primitivos del lenguaje pythom 
+"""
+Los tipos primitivos o tipos de datos en python son las estructuras de datos más elementales del lenguaje de programación. 
+Son los componentes básicos para tratar todo tipo de datos o variables y contienen valores de datos puros y simples.
+""" 
+#En python pueden ser enteros, flotantes, booleanos y cadenas o strings
+
+#Enteros (int)
+
+#Estas variables representan numeros enteros.
+
+año = 2024
+mes = 10
+dia = 8
+
+#Flotantes (float)
+
+#estas variables representan numeros con coma o decimales
+
+peso = 64,6
+estatura = 1,72
+
+#Booleanos (bool)
+
+#estas variables son binarias y representan valores de true (verdadero) y false (falso)
+
+soy_guapo_lo_se = True
+hace_calor = False
+
+#Cadenas o strings (str)
+
+# son un arreglo de caracteres que forman cadenas para formar un mensaje o oración generalmente. 
+
+titulo = "Primera velada"
+
+autor = "Rimbaud"
+
+Texto = """
+Ella estaba desnuda
+y grandes árboles indiscretos
+a los cristales arrojaban su follaje
+astutamente, muy cerca, muy cerca
+
+"""
+
+print("¡Hola, python!") 
+
