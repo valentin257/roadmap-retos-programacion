dbc595ce05ac87d3efb474a1a9f9e61ae1b345c8
valentin257 <valentin.bertonati@outlook.com>
2024-10-08 22:20:39 -0300

    Create valentin257.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/valentin257.py
@@ -0,0 +1,25 @@
+# TIPOS DE OPERADORES EN PYTHON
+
+"""
+Existen varios tipos de operadores en python:
+1. Operadores aritméticos
+2. Operadores relacionales
+3. Operadores Bit a Bit
+4. Operadores de asignación
+5. Operadores lógicos
+6. Operadores de pertenencia
+7. Operadores de identidad
+"""
+
+""" 1. Operadores aritmeticos.
+Un operador aritmetico recibe dos operandos como entrada y ha ce una operación.
+Exiten 7 tipos de operadores aritméticos: 
++ Realiza una suma entre los operandos.
+- Realiza una resta entre los operandos.
+* Realiza una multiplicacion entre los operandos.
+/ Realiza una division entre los operandos.
+% Realiza un modulo entre los operandos.
+** Realiza la potencia de los operandos.
+// Realiza una division con resultado entero entre los operandos.
+"""
+print(f"suma: 2+4 = {2+4}")
